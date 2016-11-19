772ca0ebc853fd297d78e8173b087183cd183c19
jeremycurny <jeremy.curny.pro@gmail.com>
2016-11-19 13:06:03 -0500

    Update provisioner.sh

--- files/Sysadmin/Software/Vagrant/Boxes/Alpha/provisioner.sh
+++ files/Sysadmin/Software/Vagrant/Boxes/Alpha/provisioner.sh
@@ -32,6 +32,43 @@ echo "PHP"
 apt-get install -y php5 php-apc php5-curl php5-gd php5-imap php5-intl php5-mcrypt php5-mysql php5-xdebug
 curl -o /usr/local/bin/composer https://getcomposer.org/composer.phar && chmod +x /usr/local/bin/composer
 
+# Apache configure
+echo "Apache configure"
+rm -f /var/www/index.html
+rm -f /var/www/html/index.html
+rm -f /etc/apache2/sites-enabled/*
+rm -f /etc/apache2/sites-available/*
+mkdir -p /var/www/html/vhosts
+mkdir -p /var/www/html/vhosts/default
+echo 'Deny from all' > /var/www/html/.htaccess
+echo 'Deny from all' > /var/www/html/vhosts/.htaccess
+echo 'Allow from all' > /var/www/html/vhosts/default/.htaccess
+echo 'Default Vhost' > /var/www/html/vhosts/default/index.html
+cat <<EOF > /etc/apache2/sites-available/000-default.conf
+<VirtualHost _default_:80>
+  ServerName default:80
+  DocumentRoot /var/www/html/vhosts/default
+  ErrorLog ${APACHE_LOG_DIR}/default.error.log
+  CustomLog ${APACHE_LOG_DIR}/default.access.log combined
+</VirtualHost>
+<IfModule mod_ssl.c>
+  <VirtualHost _default_:443>
+    ServerName default:80
+    DocumentRoot /var/www/html/vhosts/default
+    SSLEngine on
+    SSLCertificateFile /etc/ssl/certs/ssl-cert-snakeoil.pem
+    SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key
+    <FilesMatch "\.(shtml|phtml|php)$">
+      SSLOptions +StdEnvVars
+    </FilesMatch>
+    BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown
+    ErrorLog ${APACHE_LOG_DIR}/default.error.log
+    CustomLog ${APACHE_LOG_DIR}/default.access.log combined
+  </VirtualHost>
+</IfModule>
+EOF
+a2ensite 000-default.conf
+
 # Post install
 echo "Post install"
 service apache2 restart
