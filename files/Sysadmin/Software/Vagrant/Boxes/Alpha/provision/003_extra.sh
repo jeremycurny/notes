a0de6db825a576ac78f3e6b49d1e5a50a070df4c
jeremycurny <jeremy.curny.pro@gmail.com>
2016-11-20 10:08:46 -0500

    Update 003_extra.sh

--- files/Sysadmin/Software/Vagrant/Boxes/Alpha/provision/003_extra.sh
+++ files/Sysadmin/Software/Vagrant/Boxes/Alpha/provision/003_extra.sh
@@ -5,6 +5,11 @@ export DEBIAN_FRONTEND=noninteractive
 # Start
 echo "003_extra.sh start"
 
+## Adminer
+echo "Adminer"
+mkdir -p /var/www/html/vhosts/default/adminer
+curl -o /var/www/html/vhosts/default/adminer/index.php http://www.adminer.org/static/download/4.2.5/adminer-4.2.5-en.php
+
 ## Composer
 echo "Composer"
 curl -o /usr/local/bin/composer https://getcomposer.org/composer.phar && chmod +x /usr/local/bin/composer
