63d69aeb38d0f1d0b1c6df9174ed0d3aed7bab5a
jeremycurny <jeremy.curny.pro@gmail.com>
2017-02-08 11:44:47 -0500

    Update clean-code.sh

--- files/Dev/clean-code.sh
+++ files/Dev/clean-code.sh
@@ -7,4 +7,4 @@ find . -type f -exec dos2unix {} \;
 find . -name "*.php" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
 
 # Ensure newline at eof
-find . -name "*.php" -type f -print0 | xargs -0 sed -i -e '$a\' file
+find . -name "*.php" -type f -print0 | xargs -0 sed -i -e '$a\'
