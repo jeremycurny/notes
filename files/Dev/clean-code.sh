ce8599180849db8dbb6f39f9e7bc21b1a030e736
jeremycurny <jeremy.curny.pro@gmail.com>
2017-06-22 14:53:47 -0400

    Update clean-code.sh

--- files/Dev/clean-code.sh
+++ files/Dev/clean-code.sh
@@ -4,21 +4,22 @@
 # Trim trailing white spaces
 # Ensure newline at eof
 
-# All
-find . -type f -exec dos2unix {} \;
-
 # CSS
+find . -name "*.css" -type f -exec dos2unix {} \;
 find . -name "*.css" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
 find . -name "*.css" -type f -print0 | xargs -0 sed -i -e '$a\'
 
 # JS
+find . -name "*.js" -type f -exec dos2unix {} \;
 find . -name "*.js" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
 find . -name "*.js" -type f -print0 | xargs -0 sed -i -e '$a\'
 
 # PHP
+find . -name "*.php" -type f -exec dos2unix {} \;
 find . -name "*.php" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
 find . -name "*.php" -type f -print0 | xargs -0 sed -i -e '$a\'
 
 # PHTML
+find . -name "*.phtml" -type f -exec dos2unix {} \;
 find . -name "*.phtml" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
 find . -name "*.phtml" -type f -print0 | xargs -0 sed -i -e '$a\'
