997b161dc2e0e39ef22060ab82ea085f35efe37f
jeremycurny <jeremy.curny.pro@gmail.com>
2017-06-22 14:46:51 -0400

    Update clean-code.sh

--- files/Dev/clean-code.sh
+++ files/Dev/clean-code.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
 # DOS to UNIX (To LF)
+# Trim trailing white spaces
+# Ensure newline at eof
+
+# All
 find . -type f -exec dos2unix {} \;
 
-# Trim trailing white spaces
-find . -name "*.php" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
+# CSS
+find . -name "*.css" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
+find . -name "*.css" -type f -print0 | xargs -0 sed -i -e '$a\'
 
-# Ensure newline at eof
+# JS
+find . -name "*.js" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
+find . -name "*.js" -type f -print0 | xargs -0 sed -i -e '$a\'
+
+# PHP
+find . -name "*.php" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
 find . -name "*.php" -type f -print0 | xargs -0 sed -i -e '$a\'
+
+# PHTML
+find . -name "*.phtml" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
+find . -name "*.phtml" -type f -print0 | xargs -0 sed -i -e '$a\'
