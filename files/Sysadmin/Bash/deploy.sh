48e12e3fc55c36a38bf57228892a0edc430c4b7e
jeremycurny <jeremy.curny.pro@gmail.com>
2017-10-16 14:17:00 -0400

    Update deploy.sh

--- files/Sysadmin/Bash/deploy.sh
+++ files/Sysadmin/Bash/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -ex
 
-# Usage: deploy.sh /home/jcurny/project user@server /var/www/html
-
 # $1: SRC path (/var/www/html/)
 # $2: SSH credential (user@server)
 # $3: SRC deploy path (/var/www/html/)
@@ -25,8 +23,8 @@ now=`date '+%Y%m%d%H%M%S'`;
 zipFilename="deploy-${now}.zip"
 
 mkdir -p tmpPath
-zip -qr /tmp/deploy/${zipName} $1
+zip -qr /tmp/deploy/${zipFilename} $1
 
-ssh $2 "mkdir -p /tmp/deploy/ondeck && rm -rf /tmp/deploy/ondeck"
-scp /tmp/deploy/${zipName} $2:/tmp/deploy/ondeck/${zipName}
-ssh $2 unzip /tmp/deploy/ondeck/${zipName} && rm /tmp/deploy/ondeck/${zipName}
+ssh $2 "mkdir -p /tmp/deploy/ondeck && rm -rf /tmp/deploy/ondeck/*"
+scp /tmp/deploy/${zipFilename} $2:/tmp/deploy/ondeck/${zipFilename}
+ssh $2 "unzip -q /tmp/deploy/ondeck/${zipFilename} && rm /tmp/deploy/ondeck/${zipFilename}"
